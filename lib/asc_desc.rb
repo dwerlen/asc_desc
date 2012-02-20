0e2eed03a4632d292f431a89f8fcb18ccc69c5b0
David Werlen <werlen@icare.ch>
2012-02-20 19:29:13 +0100

    fixes issue with String#split with ruby 1.8.7 when passing a Character.

--- lib/asc_desc.rb
+++ lib/asc_desc.rb
@@ -12,8 +12,8 @@ module AscDesc
     args[0..-2].map do |column|
       if column.is_a?(Array)
         format_order_clause(*column << order)
-      elsif column.respond_to?('include?') and column.include?(?,)
-        format_order_clause(*column.split(?,) << order)
+      elsif column.respond_to?('include?') and column.include?(',')
+        format_order_clause(*column.split(',') << order)
       else
         "#{column.to_s.strip} #{order}"
       end
