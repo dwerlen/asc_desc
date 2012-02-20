79f968452aafdb0ed1f109d6e5b841266f5b9c16
David Werlen <werlen@icare.ch>
2012-02-20 19:07:28 +0100

    fixes Splat! issue with ruby 1.8.7.

--- lib/asc_desc/model_additions.rb
+++ lib/asc_desc/model_additions.rb
@@ -2,13 +2,13 @@ module AscDesc
   module ModelAdditions
 
     def asc(*args)
-      self.order AscDesc.format_order_clause(*args, AscDesc::ASC)
+      self.order AscDesc.format_order_clause(*args << AscDesc::ASC)
     end
     alias :ascending :asc
     alias :ascending_order :asc
 
     def desc(*args)
-      self.order AscDesc.format_order_clause(*args, AscDesc::DESC)
+      self.order AscDesc.format_order_clause(*args << AscDesc::DESC)
     end
     alias :descending :desc
     alias :descending_order :desc
